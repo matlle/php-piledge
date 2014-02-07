451ab265ec74343c085cdf808fa62fb5cac02484
Martial Babo <paso.175@gmail.com>
2014-02-07 23:13:40 +0000

    Initial project version

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 include_once('Twig/lib/Twig/Autoloader.php');
     Twig_Autoloader::register();
                  
-    $loader = new Twig_Loader_Filesystem('View'); // Dossier contenant les templates
+    $loader = new Twig_Loader_Filesystem('View');
     $twig = new Twig_Environment($loader, array(
             'cache' => false
     ));
